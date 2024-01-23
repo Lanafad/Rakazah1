31cc4bb3d02a9f766b8e6a6c2dd1679f1d67293c
lana <lana.alfaadhel1@gmail.com>
2024-01-23 14:49:27 +0300

    Initial Commit

---
+++ Rakazah/RakazahApp.swift
@@ -0,0 +1,17 @@
+//
+//  RakazahApp.swift
+//  Rakazah
+//
+//  Created by lana alfaadhel on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RakazahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
