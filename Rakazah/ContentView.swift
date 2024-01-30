94aaaea9d7e51a675194fa0983e3113520e8667e
leena <leena1759@hotmail.com>
2024-01-30 15:50:26 +0300

    first edit

--- Rakazah/ContentView.swift
+++ Rakazah/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+@State private var isShowingSheet = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world! do you see me?")
+        
+        ZStack {
+            Color("RBeige").edgesIgnoringSafeArea(.all)
+           
+            Text("المهام")
+                .font(.system(size:32))
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .leading)
+                
+                
+            Button(action: {
+                isShowingSheet = true
+            }) {
+                Text("New Task")
+                    .font(.headline)
+                    .frame(width: 300, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color("RBrown"))
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 4)
+            }
+            .padding()
+            .sheet(isPresented: $isShowingSheet) {
+            
+            }
         }
-        .padding()
+        
     }
 }
 
