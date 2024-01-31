3337124b5edd2c51c06177bb05cf809999931db7
SHOUQ9HUB <143387953+SHOUQ9HUB@users.noreply.github.com>
2024-01-31 17:13:29 +0300

    a

---
+++ Rakazah/CloudKitBootcamps/CloudKitUserBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CloudKitUserBootcamp.swift
+//  Rakazah
+//
+//  Created by shouq on 19/07/1445 AH.
+//
+
+import SwiftUI
+
+struct CloudKitUserBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CloudKitUserBootcamp()
+}
