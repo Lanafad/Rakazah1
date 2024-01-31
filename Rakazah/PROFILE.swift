9c6ef7788133114fa156f38236e1e7fd8100f84f
lujainalra <l_i_alrasheed@hotmail.com>
2024-01-31 17:46:21 +0300

    new

--- Rakazah/PROFILE.swift
+++ Rakazah/PROFILE.swift
@@ -10,19 +10,18 @@ import SwiftUI
 struct PROFILE: View {
     @State private var username: String = ""
     @State private var name: String = ""
+    
     var body: some View {
-        ZStack{
-            
+        ZStack {
             Color("RBeige")
                 .ignoresSafeArea()
             
-       
-            
-            VStack{
+            VStack {
                 Text("لينا")
                     .font(.system(size: 34))
                     .fontWeight(.semibold)
                     .frame(alignment: .topLeading)
+                
                 Circle()
                     .frame(width: 129, height: 131)
                     .padding(.bottom, 22)
@@ -31,41 +30,31 @@ struct PROFILE: View {
                     .multilineTextAlignment(.trailing)
                     .frame(width: 390, height: 50, alignment: .topLeading)
                     .background(.white)
-                
                     .cornerRadius(20)
-                
+                    .padding(.bottom, 20)
                 
                 TextField(" اسم الحساب", text: $name)
                     .multilineTextAlignment(.trailing)
                     .frame(width: 390, height: 50, alignment: .topLeading)
                     .background(.white)
-                
                     .cornerRadius(20)
+                    .padding(.bottom, 40) // Add space of 40 between button and text fields
                 
-                ZStack{
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("حفظ")
-                            .foregroundStyle(Color.white)/*.padding(.trailing, 200)*/
-                                .font(.system(size: 24))
-                    })
-                    .frame(width: 327, height: 57)
-                    .background{
-                        Color("RBrown")
-                    }
-                    .cornerRadius(20)
-                   
-                    
-
-                   
-                    
-                    
-                    
-                }
+                Button(action: {}, label: {
+                    Text("حفظ")
+                        .foregroundStyle(Color.white)
+                        .font(.system(size: 24))
+                })
+                .frame(width: 327, height: 57)
+                .background(Color("RBrown"))
+                .cornerRadius(20)
+                .padding(.top, 22)
                 
+                Spacer()
             }
         }
-    }}
+    }
+}
 
 #Preview {
     PROFILE()
--- Rakazah/groupTasks.swift
+++ Rakazah/groupTasks.swift
@@ -6,13 +6,136 @@
 //
 
 import SwiftUI
+//import Modals
 
 struct groupTasks: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @State  private var showSheet : Bool = false
+    var body : some View {
+        ZStack{
+            Color("RBeige")
+                .ignoresSafeArea()
+            
+            Button ("+"){
+                
+                showSheet.toggle()
+            }
+            
+            .sheet(isPresented: $showSheet){
+                mysheet()
+                    .presentationDetents([.height(628), .medium, .large])
+
+            }}}}
+struct mysheet :View {
+    @State private var username: String = ""
+    @State private var birthDate = Date.now
+    @State private var currentDate = Date()
+    @State private var selection = "Red"
+    let members = [""]
+    var body:some View{
+        VStack {
+            Text("إنشاء المهمة")
+                .font(.system(size: 22))
+                .fontWeight(.semibold)
+                .padding(.leading, 250)
+                .padding(.top, 22)
+            // .padding(.bottom, 150)
+            
+            HStack {
+               
+                Text("اسم المهمة")
+                    .font(.system(size: 22))
+                    .padding(.top, 15)
+                    .padding(.leading,270)
+                    
+                
+            }
+            
+            TextField("مثال: تغسيل ملابس", text: $username)
+               
+                .padding(.leading,250)
+          
+            Text("المكافأة")
+                .font(.system(size: 22))
+                .padding(.top, 15)
+                .padding(.leading,300)
+            
+            TextField(" مثال: عزيمة عشاء", text: $username)
+                .padding(.leading,250)
+            
+           
+            
+        }
+        VStack {
+            HStack {
+               
+                Text("موعد التسليم")
+                    .font(.system(size: 22))
+                    .padding(.top, 15)
+                    .padding(.leading,250)
+                
+            }
+            
+            HStack {
+                DatePicker("", selection: $currentDate, displayedComponents: .date)
+                    .frame(maxWidth: .infinity)
+                
+                DatePicker("", selection: $currentDate, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+            }
+            .padding(.horizontal,40)
+        }
+        
+               .pickerStyle(.menu)
+        
+        
+        HStack{
+            
+            Text("صاحب المهمة")
+                .font(.system(size: 22))
+                .padding(.top, 15)
+                .padding(.leading,240)
+      
+            Picker("Select a member", selection: $selection) {
+                ForEach(members, id: \.self) {
+                    Text($0)
+                }
+            }
+            .pickerStyle(.menu)
+            .padding(.leading,-220)
+            .padding(.top, 17)
+            
+            
+            
+        }
+        
+        Spacer()
+        Button(action: { }, label: {
+            ZStack{
+                Text("إنشاء").foregroundStyle(Color.white)/*.padding(.trailing, 200)*/
+                    .font(.system(size: 24))
+                
+            }
+            .padding()
+            .frame(width: 327, height: 57)
+            .background{
+                Color("RBrown")
+                
+                
+                
+            }
+            .cornerRadius(20)
+            
+        })
+        
+        
     }
 }
+    
+    
 
-#Preview {
-    groupTasks()
-}
+    
+    #Preview {
+        groupTasks()
+    }
+    
+
