a4e32049883d8a33f532ea2fbe7c0ba1e00bc30d
lana <lana.alfaadhel1@gmail.com>
2024-02-02 01:21:25 +0300

    initial merge

---
+++ Rakazah/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  Rakazah
+//
+//  Created by lana alfaadhel on 02/02/2024.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Profile()
+}
